5cade4a7078b0139fde041149e1537688cbcad05
Philip Zhang <zhangxilai@doublechaintech.com>
2021-06-09 02:03:40 +0800

    Initial version

---
+++ todo-cli/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+
+    let action = std::env::args().nth(1).expect("Please specify an action");
+    let item = std::env::args().nth(2).expect("Please specify an item");
+
+    println!("{:?}, {:?}", action, item);
+    println!("Hello, world!");
+}
